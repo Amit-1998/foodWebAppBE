14009178a84b39c4ca2360edd0673ce206ab99a5
Amit-1998 <amitkumar654521@gmail.com>
2022-01-24 15:04:20 +0530

    process.env

--- controller/authController.js
+++ controller/authController.js
@@ -2,8 +2,7 @@ const express = require("express");
 const userModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const { sendMail } = require("../utility/nodemailer");
-const { JWT_SECRET } = process.env || require("../secrets");
-
+const { JWT_SECRET } = process.env;
 //sign up user
 module.exports.signup = async function signup(req, res) {
   try {

34f861c7f88a4fb8980e3baf7ed00f1ac0f39e6d
Amit-1998 <amitkumar654521@gmail.com>
2022-02-12 19:34:47 +0530

    allow credentials: false

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const cors = require("cors");
+
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
-
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Allow-credentials",false);
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OTIONS");
+  next();
+});
+//app.use(cors())
 const mongoose=require('mongoose');
 const { PASSWORD } = process.env;
 const db_link = `mongodb+srv://AmitfoodApp:${PASSWORD}@cluster0.lwgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+var cors = require('cors');
 
 app.use(express.static('public/build'));
 
